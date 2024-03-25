b63ea12f952bdc595b390935186e29fd18974df8
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-25 14:31:07 -0300

    update

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'hero'
 
 describe Hero do
-    it "has a sword" do
+    it 'has a sword' do
         hero = Hero.new
         expect(hero.weapon).to eq('sword')
     end
